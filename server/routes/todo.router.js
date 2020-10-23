24ee25f4e28d94bffbeeb30fa35aad626a9f4516
Zane Harvell <zane.harvell@gmail.com>
2020-10-23 16:31:16 -0500

    creating postroute

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-  database: 'shoestore', // name of database
+  database: 'todolist', // name of database
   host: 'localhost',
   port: 5432,
   max: 10, // max number of concurrent connections
@@ -22,3 +22,21 @@ pool.on('connect', () => {
 pool.on('error', (error) => {
   console.log('Error connecting to db', error);
 });
+
+router.post('/', function (req, res) {
+  const taskToAdd = req.body;
+  console.log('in POST route', taskToAdd);
+  const query = 'INSERT INTO "ToDoList" ("title", "status") VALUES ($1, $2);';
+
+  pool
+    .query(query, [taskToAdd.title, taskToAdd.status])
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('ERROR in POST', error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
